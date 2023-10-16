a431ae5baa1322a5f753345ea9954244e2d84cc0
leonardbay <36239260+leonardbay@users.noreply.github.com>
2023-10-16 12:20:45 -0400

    a

--- eello-b2c/main.js
+++ eello-b2c/main.js
@@ -1,45 +1,12 @@
-function modifyActionButton(button, icon, text) {
-    if (text === undefined) {
-        text = button.innerText
-    }
-
-    button.classList.add('button', 'is-flex-grow-1')
-    button.classList.add('btn', 'btn-dark')
-
-    const iconSpan = document.createElement('span')
-    iconSpan.classList.add('icon', 'mx-1')
-    iconSpan.innerHTML = `<i class="${icon}"></i>`
-
-    const textSpan = document.createElement('span')
-    textSpan.classList.add('mx-1')
-    textSpan.innerText = text
-
-    button.replaceChildren(iconSpan, textSpan)
+function showPage() {
+    document.querySelector('body').classList.remove('is-invisible')
 }
 
-function styleSocialSignInPage() {
-    const unifiedSelectorDiv = document.querySelector('#SocialSignIn #api[data-name="Unified"]')
-    if (unifiedSelectorDiv) {
-        unifiedSelectorDiv.classList.add('card', 'p-5')
-        unifiedSelectorDiv.querySelector('.claims-provider-list-buttons > .options')
-            .childNodes
-            .forEach((e) => {
-                if (e.nodeType === Node.ELEMENT_NODE) {
-                    e.classList.add('is-flex', 'mb-2', 'px-6', 'mx-7')
-                }
-            })
-
-        const googleAccountButton = unifiedSelectorDiv.querySelector('.accountButton#GoogleExchange')
-        modifyActionButton(googleAccountButton, 'fab fa-google', 'Sign in with Google')
-
-        const amazonAccountButton = unifiedSelectorDiv.querySelector('.accountButton#AmazonExchange')
-        modifyActionButton(amazonAccountButton, 'fab fa-amazon', 'Login with Amazon')
-
-        const appleAccountButton = unifiedSelectorDiv.querySelector('.accountButton#AppleExchange')
-        modifyActionButton(appleAccountButton, 'fab fa-apple', 'Sign in with Apple')
-
-        const facebookAccountButton = unifiedSelectorDiv.querySelector('.accountButton#FacebookExchange')
-        modifyActionButton(facebookAccountButton, 'fab fa-facebook-f', 'Facebook')
+function makeCopyrightText() {
+    const copyrightText = document.getElementById("copyright-text");
+    if (copyrightText) {
+        const year = new Date().getFullYear();
+        copyrightText.innerHTML = `&copy; <strong>eello</strong> ${year}. All rights reserved.`
     }
 }
 
@@ -85,6 +52,103 @@ function styleCreateAccount(form) {
                 }
             }
         }*/
+    }
+}
+
+function modifyActionButton(button, icon, text) {
+    if (text === undefined) {
+        text = button.innerText
+    }
+
+    button.classList.add('button', 'is-flex-grow-1')
+    button.classList.add('btn', 'btn-dark')
+
+    const iconSpan = document.createElement('span')
+    iconSpan.classList.add('icon', 'mx-1')
+    iconSpan.innerHTML = `<i class="${icon}"></i>`
+
+    const textSpan = document.createElement('span')
+    textSpan.classList.add('mx-1')
+    textSpan.innerText = text
+
+    button.replaceChildren(iconSpan, textSpan)
+}
+
+function styleErrorElement(element) {
+
+    const messageElement = (message) => {
+        const body = document.createElement('div')
+        body.classList.add('message-body')
+        body.innerText = message
+        return body
+    }
+
+    if (element) {
+        element.classList.add('alert', 'alert-danger', 'align-items-center')
+        const messageBody = element.querySelector('div')
+        if (!messageBody) {
+            const errorMessage = element.innerText
+            element.replaceChildren(messageElement(errorMessage))
+        }
+    }
+}
+
+function makePwdToggler(pwd) {
+    const  div = document.createElement('div');
+    div.classList.add('form-check', 'form-switch', 'my-4')
+
+    const  checkbox = document.createElement('input');
+    checkbox.setAttribute('type', 'checkbox');
+    const  id = pwd.id + 'toggler';
+    checkbox.setAttribute('id', id);
+    checkbox.classList.add('form-check-input', 'mr-2')
+    div.appendChild(checkbox);
+    
+    const label = document.createElement('label');
+    label.setAttribute('for', id);
+    label.appendChild(document.createTextNode('Show Password'));
+    label.classList.add('form-check-label', 'mx-2')
+    div.appendChild(label);    
+
+    pwd.insertAdjacentElement('afterend', div);
+
+    const toggle = () => {
+        pwd.type = pwd.type === 'password' ? 'text' : 'password';
+    }
+    checkbox.addEventListener('click', toggle)
+    checkbox.addEventListener('keydown', toggle)
+}
+
+function setupPwdTogglers() {
+    var pwdInputs = document.querySelectorAll('input[type=password]');
+    for (var i = 0; i < pwdInputs.length; i++) {
+        makePwdToggler(pwdInputs[i]);
+    }
+}
+
+function styleSocialSignInPage() {
+    const unifiedSelectorDiv = document.querySelector('#SocialSignIn #api[data-name="Unified"]')
+    if (unifiedSelectorDiv) {
+        unifiedSelectorDiv.classList.add('card', 'p-5')
+        unifiedSelectorDiv.querySelector('.claims-provider-list-buttons > .options')
+            .childNodes
+            .forEach((e) => {
+                if (e.nodeType === Node.ELEMENT_NODE) {
+                    e.classList.add('is-flex', 'mb-2', 'px-6', 'mx-7')
+                }
+            })
+
+        const googleAccountButton = unifiedSelectorDiv.querySelector('.accountButton#GoogleExchange')
+        modifyActionButton(googleAccountButton, 'fab fa-google', 'Sign in with Google')
+
+        const amazonAccountButton = unifiedSelectorDiv.querySelector('.accountButton#AmazonExchange')
+        modifyActionButton(amazonAccountButton, 'fab fa-amazon', 'Login with Amazon')
+
+        const appleAccountButton = unifiedSelectorDiv.querySelector('.accountButton#AppleExchange')
+        modifyActionButton(appleAccountButton, 'fab fa-apple', 'Sign in with Apple')
+
+        const facebookAccountButton = unifiedSelectorDiv.querySelector('.accountButton#FacebookExchange')
+        modifyActionButton(facebookAccountButton, 'fab fa-facebook-f', 'Facebook')
     }
 }
 
@@ -144,25 +208,6 @@ function styleLinkSocialLoginPage() {
 
         const facebookAccountButton = unifiedSelectorDiv.querySelector('.accountButton#FacebookExchange')
         modifyActionButton(facebookAccountButton, 'fab fa-facebook-f', 'Facebook')
-    }
-}
-
-function styleErrorElement(element) {
-
-    const messageElement = (message) => {
-        const body = document.createElement('div')
-        body.classList.add('message-body')
-        body.innerText = message
-        return body
-    }
-
-    if (element) {
-        element.classList.add('alert', 'alert-danger', 'align-items-center')
-        const messageBody = element.querySelector('div')
-        if (!messageBody) {
-            const errorMessage = element.innerText
-            element.replaceChildren(messageElement(errorMessage))
-        }
     }
 }
 
@@ -252,49 +297,46 @@ function styleLocalSignUpPage() {
     }
 }
 
-function makePwdToggler(pwd) {
-    const  div = document.createElement('div');
-    div.classList.add('form-check', 'form-switch', 'my-4')
+function styleGALinkPage() {
+    const localSignInDiv = document.querySelector('#GALink #api[data-name="Unified"]')
+    if (localSignInDiv) {
+        localSignInDiv.classList.add('card', 'p-5')
+        /*const intro = localSignInDiv.querySelector('.intro');
 
-    const  checkbox = document.createElement('input');
-    checkbox.setAttribute('type', 'checkbox');
-    const  id = pwd.id + 'toggler';
-    checkbox.setAttribute('id', id);
-    checkbox.classList.add('form-check-input', 'mr-2')
-    div.appendChild(checkbox);
-    
-    const label = document.createElement('label');
-    label.setAttribute('for', id);
-    label.appendChild(document.createTextNode('Show Password'));
-    label.classList.add('form-check-label', 'mx-2')
-    div.appendChild(label);    
+        if(intro) {
+            intro.remove();
+        }*/
 
-    pwd.insertAdjacentElement('afterend', div);
+        const form = localSignInDiv.querySelector('form')
 
-    const toggle = () => {
-        pwd.type = pwd.type === 'password' ? 'text' : 'password';
+        if(form) {
+            form.classList.add('is-flex', 'is-flex-direction-column')
+            form.childNodes.forEach((e) => {
+                if (e.nodeType === Node.ELEMENT_NODE) {
+                    e.classList.add('m-2')
+                }
+            })
+
+            const errorDivs = localSignInDiv.querySelectorAll('.error')
+            errorDivs.forEach(errorDiv => {
+                styleErrorElement(errorDiv)
+            })
+
+            setupPwdTogglers();
+
+            form.querySelector('#signInName').classList.add('form-control', 'my-2')
+
+            form.querySelector('#password').classList.add('form-control', 'my-2')
+            //form.querySelector('#forgotPassword').classList.add('btn', 'btn-outline-danger', 'my-4')
+
+            const nextElement = form.querySelector('#next');
+
+            if(nextElement) {
+                nextElement.classList.add('btn', 'btn-dark', 'my-5')
+            }
+        }
     }
-    checkbox.addEventListener('click', toggle)
-    checkbox.addEventListener('keydown', toggle)
-}
-
-function setupPwdTogglers() {
-    var pwdInputs = document.querySelectorAll('input[type=password]');
-    for (var i = 0; i < pwdInputs.length; i++) {
-        makePwdToggler(pwdInputs[i]);
-    }
-}
-
-function makeCopyrightText() {
-    const copyrightText = document.getElementById("copyright-text");
-    if (copyrightText) {
-        const year = new Date().getFullYear();
-        copyrightText.innerHTML = `&copy; <strong>eello</strong> ${year}. All rights reserved.`
-    }
-}
-
-function showPage() {
-    document.querySelector('body').classList.remove('is-invisible')
+    makeCopyrightText()
 }
 
 function stylePages() {
@@ -302,6 +344,7 @@ function stylePages() {
     styleLocalSignInPage();
     styleLocalSignUpPage();
     styleLinkSocialLoginPage();
+    styleGALinkPage();
     makeCopyrightText();
     showPage();
 }
